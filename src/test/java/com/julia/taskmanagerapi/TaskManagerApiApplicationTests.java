7c0d78b209a65e5aa4c8f7135dd541ceae81116e
juliateles99 <juliateles.github@gmail.com>
2025-04-30 23:42:05 -0300

    initial commit

---
+++ src/test/java/com/julia/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.taskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
